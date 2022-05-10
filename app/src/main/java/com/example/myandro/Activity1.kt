2ae2d56fd0d2d8969fc5f69b8d64a903a727ea16
borsuk <tomaszjanrys@gmail.com>
2022-05-10 13:49:50 -0400

    zajecia 3

--- app/src/main/java/com/example/myandro/Activity1.kt
+++ app/src/main/java/com/example/myandro/Activity1.kt
@@ -1,6 +1,8 @@
 package com.example.myandro
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -13,7 +15,12 @@ class Activity1 : AppCompatActivity() {
         val txtViewLayout2 : TextView = findViewById(R.id.textViewLayout2)
         txtViewLayout2.setText("Nowe okno")
 
-
+        //btn back to start
+        val btnBtoStart : Button = findViewById(R.id.btnBackMainAcvID)
+        btnBtoStart.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 
