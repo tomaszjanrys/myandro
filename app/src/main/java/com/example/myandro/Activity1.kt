99d02d3050e36351b5bf2653eda752cdaa849329
borsuk <tomaszjanrys@gmail.com>
2022-05-10 14:21:53 -0400

    zajecia 3 end

--- app/src/main/java/com/example/myandro/Activity1.kt
+++ app/src/main/java/com/example/myandro/Activity1.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myandro.databinding.Activity1Binding
 
 class Activity1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +22,14 @@ class Activity1 : AppCompatActivity() {
         btnBtoStart.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+
+
+        }
+        val biding = Activity1Binding.inflate(layoutInflater)
+        setContentView(biding.root)
+
+        biding.btnTestBIDDING.setOnClickListener{
+            Toast.makeText(this,"to jest biding", Toast.LENGTH_SHORT).show()
         }
     }
 
