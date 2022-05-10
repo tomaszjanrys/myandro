ead74ecbe1a5a07b1c54576f53f7fd6150861f7d
borsuk <tomaszjanrys@gmail.com>
2022-05-09 23:43:01 -0400

    myAndro

---
+++ app/src/main/java/com/example/myandro/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myandro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity1)
+
+        val myTextView1: TextView = findViewById(R.id.myTextView)
+        myTextView1.setText("bowy text")
+
+        val myTextView2: TextView = findViewById(R.id.myTextView2)
+        myTextView2.setText("Kolejny text")
+    }
+
+
+
+}
